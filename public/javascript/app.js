1765ab2dd731897beea2510e18b35b0ca16a3f6e
Milo Knowles <mknowles@mit.edu>
2017-10-08 13:45:00 -0400

    Basic layout and front end routing

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,8 +3,9 @@ menuBar.className = 'menu-bar';
 menuBar.innerHTML =
 '<a class="active" href="#about">About</a>\
 <a href="#projects">Projects</a>\
+<a href="#contact">Contact</a>\
 <a href="/images/resume.pdf">Resume</a>\
-<a href="https://github.com/milokhl/rl-poker-app">Github</a>';
+<a href="https://github.com/milokhl">Github</a>';
 document.body.appendChild(menuBar);
 
 menuBar.addEventListener('click', function(e) {
@@ -13,6 +14,8 @@ menuBar.addEventListener('click', function(e) {
 
 var aboutSection = document.getElementById('about');
 var projectSection = document.getElementById('projects');
+var contactSection = document.getElementById('contact');
+contactSection.hidden = true;
 projectSection.hidden = true;
 
 // Set up about page.
@@ -32,9 +35,16 @@ projectText.innerHTML = "Some of my projects are listed below:";
 projectSection.appendChild(projectsTitle);
 projectSection.appendChild(projectText);
 
+// Set up contact section.
+contactSection.innerHTML = '<h1>Contact</h1>\
+	<p>Milo Knowles</p>\
+	<p>mknowles@mit.edu</p>\
+	<p>hmu on linkedin</p>';
+
 var pages = {
 	'about': aboutSection,
 	'projects': projectSection,
+	'contact': contactSection
 };
 
 function navigate() {
