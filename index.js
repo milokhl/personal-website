764dade7f470d503daa5adb8024197d04cf369ec
Milo Knowles <mknowles@mit.edu>
2018-10-12 11:34:59 -0400

    Refactor

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var express 					= require('express');
 		browserify 				= require('browserify-middleware');
 
 var publicDir = path.join(__dirname, './public');
+app.locals.basedir = publicDir; // Needed for pug to use absolute paths.
 
 // Set the view engine to serve .pug files from /views.
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 
 // Serve static HTML.
 app.use('/', express.static(publicDir));
@@ -16,10 +16,20 @@ app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port', process.env.PORT || 3000);
 });
 
+// Serve the homepage at the base URL.
 app.get("/", (req, res) => {
-  res.render("homepage", {
-    user: req.user
-  });
+  res.render("homepage", {});
+});
+
+// Serve projects by string matching.
+app.get("/projects/*", (req, res) => {
+	const pageUrl = req.params['0'];
+	if (pageUrl) {
+		const projectUrl = path.join('projects', pageUrl.replace('.md', '.pug'));
+		res.render(projectUrl); // Get rid of .md so that the .pug page is loaded.
+	} else {
+		res.render("project", {}); // Else show the base project page.
+	}
 });
 
 // Let's Encrypt challenge.
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,12 +1,1 @@
-var marked = require('marked');
-console.log(marked('I am using __markdown__.'));
-marked.setOptions({
-  renderer: new marked.Renderer(),
-  gfm: true,
-  tables: true,
-  breaks: false,
-  pedantic: false,
-  sanitize: true,
-  smartLists: true,
-  smartypants: false
-});
+console.log("Loaded index.js");
